b036c71a5484d12aab792f94572411160d156121
Ciprian Sabolovits <ciprian.sabolovits@cognosante.com>
2018-08-27 11:30:52 -0400

    Parametrize port for frontend app

--- web_frontend/web_frontend.js
+++ web_frontend/web_frontend.js
@@ -3,31 +3,31 @@ var app = express();
 var redis = require('redis');
 
 var client = redis.createClient(6379, 'redis');
-client.on("error", function (err) {
-    console.error("Redis error", err);
+client.on('error', function(err) {
+  console.error('Redis error', err);
 });
 
-app.get('/', function (req, res) {
-    res.redirect('/index.html');
+app.get('/', function(req, res) {
+  res.redirect('/index.html');
 });
 
-app.get('/json', function (req, res) {
-    client.hlen('wallet', function (err, coins) {
-        client.get('hashes', function (err, hashes) {
-            var now = Date.now() / 1000;
-            res.json( {
-                coins: coins,
-                hashes: hashes,
-                now: now
-            });
-        });
+app.get('/json', function(req, res) {
+  client.hlen('wallet', function(err, coins) {
+    client.get('hashes', function(err, hashes) {
+      var now = Date.now() / 1000;
+      res.json({
+        coins: coins,
+        hashes: hashes,
+        now: now
+      });
     });
+  });
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-var server = app.listen(80, function () {
-    console.log('web_frontend running on port 80');
+var server = app.listen(process.env.APP_PORT || 80, function() {
+  console.log('web_frontend running on port 80');
 });
 
-module.exports = server
+module.exports = server;
