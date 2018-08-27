7de0e213c0418d9d666774c03a9e3bc3472d7b5d
Ciprian Sabolovits <ciprian.sabolovits@cognosante.com>
2018-08-27 16:55:19 -0400

    Updated api

--- api/api_service.py
+++ api/api_service.py
@@ -9,6 +9,25 @@ import time
 
 DEBUG = os.environ.get("DEBUG", "").lower().startswith("y")
 
+
+def getService(serviceName):
+    name = serviceName.upper
+    service = {
+        NAME: name,
+        HOST: os.environ.get("{}_SERVICE_HOST".format(name)),
+        PORT: os.environ.get("{}_SERVICE_PORT".format(name)),
+    }
+    if 'PORT' in service:
+        service['URL'] = 'http://{}'.format(service['HOST'])
+    else:
+        service['URL'] = 'http://{}:{}'.format(service['HOST'], service['PORT'])
+
+    print('Service: ', name)
+    for x in service:
+        print(x, ':', service[x])
+    return service
+
+
 log = logging.getLogger(__name__)
 if DEBUG:
     logging.basicConfig(level=logging.DEBUG)
@@ -16,17 +35,23 @@ else:
     logging.basicConfig(level=logging.INFO)
     logging.getLogger("requests").setLevel(logging.WARNING)
 
-redis = Redis("redis")
+redisService = getService('REDIS_SERVICE')
+redis = redis.Redis(
+    host=os.environ.get('HOST', 'redis'),
+    port=os.environ.get('PORT', 6379),
+    password=os.environ.get('REDIS_PASSWORD')
+)
+
 
 def get_urandom():
     """calls rng to get 32 byte octet"""
-    r = requests.get("http://rng_service/32")
+    r = requests.get("{}/32".format(getService('RNG_SERVICE').get('URL', 'http://rng_service/')))
     return r.content
 
 
 def get_hash(data):
     """calls hash_service to get a random SHA1 hash"""
-    r = requests.post("http://hash_service/",
+    r = requests.post(getService('HASH_SERVICE').get('URL', 'http://hash_service/'),
                       data=data,
                       headers={"Content-Type": "application/octet-stream"})
     hex_hash = r.text
